7513b0d12284cd121d8d71122785b6ba4b133b37
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-10-17 17:41:28 +0700

    feat: add `tailwindcss` and path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		react({
 			babel: {
 				plugins: [
@@ -14,4 +17,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			"-": path.resolve(__dirname, "./src"),
+		},
+	},
 });
